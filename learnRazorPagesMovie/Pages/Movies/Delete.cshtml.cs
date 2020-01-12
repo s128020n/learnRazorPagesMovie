b62ff2beef3c3b6145e0d35f69e81d7b88d38eee
s128020@gmail.com <s128020@gmail.com>
2020-01-12 15:05:47 +0800

    使用資料庫和 ASP.NET Core https://docs.microsoft.com/zh-tw/aspnet/core/tutorials/razor-pages/sql?view=aspnetcore-3.1&tabs=visual-studio

---
+++ learnRazorPagesMovie/Pages/Movies/Delete.cshtml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using learnRazorPagesMovie.Data;
+using learnRazorPagesMovie.Models;
+
+namespace learnRazorPagesMovie
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly learnRazorPagesMovie.Data.learnRazorPagesMovieContext _context;
+
+        public DeleteModel(learnRazorPagesMovie.Data.learnRazorPagesMovieContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Movie Movie { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Movie = await _context.Movie.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (Movie == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Movie = await _context.Movie.FindAsync(id);
+
+            if (Movie != null)
+            {
+                _context.Movie.Remove(Movie);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
